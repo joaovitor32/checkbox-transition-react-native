8f130e06e332a0c36fd5f8a634d021fd3c1b9f4a
joaovitor32 <joaovitormunizlopes@gmail.com>
2022-02-09 20:47:46 -0300

    feat: updating index.tsx

--- index.tsx
+++ index.tsx
@@ -1,27 +1,2 @@
-//import Checkbox from './src/index';
-//export default Checkbox;
-import React, { useState } from 'react';
-import { View } from 'react-native';
-
-import Checkbox from './src';
-
-const CheckboxContent = (): any => {
-  const [state, setState] = useState<boolean | string>(false);
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Checkbox
-        value={state}
-        checkedColor={'#ffc800'}
-        uncheckedColor={'#800080'}
-        checkHeight={170}
-        checkWidth={180}
-        onChange={setState}
-        animationTime={150}
-        disabled={false}
-      />
-    </View>
-  );
-};
-
-export default CheckboxContent;
+import Checkbox from './src/index';
+export default Checkbox;
